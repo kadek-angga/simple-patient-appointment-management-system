be1344e43f65a62e8abc80a6783644747e9a6ac9
kadekangga <anggadarma316@gmail.com>
2024-02-17 17:32:16 +0800

    Adding Doctor Pages such as Edit and Show Pages

---
+++ resources/js/Pages/Doctors/Edit.vue
@@ -0,0 +1,218 @@
+<script setup>
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+import { Head, Link, useForm } from "@inertiajs/vue3";
+
+const props = defineProps({
+    doctor: Object,
+});
+
+const form = useForm({
+    first_name: props.doctor.first_name,
+    last_name: props.doctor.last_name,
+    specialization: props.doctor.specialization,
+    phone_number: props.doctor.phone_number,
+    email: props.doctor.email,
+});
+
+const submit = () => {
+    form.put(route("doctors.update", props.doctor.id));
+};
+</script>
+<template>
+    <Head title="Doctor" />
+    <AuthenticatedLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Edit Doctor
+            </h2>
+        </template>
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 bg-white border-b border-gray-200">
+                        <div className="flex items-center justify-between mb-6">
+                            <Link
+                                className="px-6 py-2 text-white bg-blue-500 rounded-md focus:outline-none"
+                                :href="route('doctors.index')"
+                            >
+                                Back
+                            </Link>
+                        </div>
+                        <form @submit.prevent="submit">
+                            <div class="space-y-12">
+                                <div class="border-b border-gray-900/10 pb-12">
+                                    <h2
+                                        class="text-base font-semibold leading-7 text-gray-900"
+                                    >
+                                        Personal Information
+                                    </h2>
+                                    <p
+                                        class="mt-1 text-sm leading-6 text-gray-600"
+                                    >
+                                        Use a permanent address where you can
+                                        receive mail.
+                                    </p>
+
+                                    <div
+                                        class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6"
+                                    >
+                                        <div class="sm:col-span-3">
+                                            <label
+                                                for="first-name"
+                                                class="block text-sm font-medium leading-6 text-gray-900"
+                                                >First name</label
+                                            >
+                                            <div class="mt-2">
+                                                <input
+                                                    type="text"
+                                                    name="first-name"
+                                                    id="first-name"
+                                                    autocomplete="given-name"
+                                                    v-model="form.first_name"
+                                                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                                />
+                                                <span
+                                                    className="text-red-600"
+                                                    v-if="
+                                                        form.errors.first_name
+                                                    "
+                                                >
+                                                    {{ form.errors.first_name }}
+                                                </span>
+                                            </div>
+                                        </div>
+
+                                        <div class="sm:col-span-3">
+                                            <label
+                                                for="last-name"
+                                                class="block text-sm font-medium leading-6 text-gray-900"
+                                                >Last name</label
+                                            >
+                                            <div class="mt-2">
+                                                <input
+                                                    type="text"
+                                                    name="last-name"
+                                                    id="last-name"
+                                                    autocomplete="family-name"
+                                                    v-model="form.last_name"
+                                                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                                />
+                                                <span
+                                                    className="text-red-600"
+                                                    v-if="form.errors.last_name"
+                                                >
+                                                    {{ form.errors.last_name }}
+                                                </span>
+                                            </div>
+                                        </div>
+
+                                        <div class="col-span-full">
+                                            <label
+                                                for="specializations"
+                                                class="block text-sm font-medium leading-6 text-gray-900"
+                                                >Specialization</label
+                                            >
+                                            <div class="mt-2">
+                                                <input
+                                                    type="text"
+                                                    name="specialization"
+                                                    id="specialization"
+                                                    autocomplete="specialization"
+                                                    v-model="
+                                                        form.specialization
+                                                    "
+                                                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                                />
+                                                <span
+                                                    className="text-red-600"
+                                                    v-if="
+                                                        form.errors
+                                                            .specialization
+                                                    "
+                                                >
+                                                    {{
+                                                        form.errors
+                                                            .specialization
+                                                    }}
+                                                </span>
+                                            </div>
+                                        </div>
+
+                                        <div class="col-span-full">
+                                            <label
+                                                for="phone_number"
+                                                class="block text-sm font-medium leading-6 text-gray-900"
+                                                >Phone Number</label
+                                            >
+                                            <div class="mt-2">
+                                                <input
+                                                    type="text"
+                                                    name="phone_number"
+                                                    id="phone_number"
+                                                    autocomplete="phone_number"
+                                                    v-model="form.phone_number"
+                                                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                                />
+                                                <span
+                                                    className="text-red-600"
+                                                    v-if="
+                                                        form.errors.phone_number
+                                                    "
+                                                >
+                                                    {{
+                                                        form.errors.phone_number
+                                                    }}
+                                                </span>
+                                            </div>
+                                        </div>
+
+                                        <div class="sm:col-span-4">
+                                            <label
+                                                for="email"
+                                                class="block text-sm font-medium leading-6 text-gray-900"
+                                                >Email address</label
+                                            >
+                                            <div class="mt-2">
+                                                <input
+                                                    id="email"
+                                                    name="email"
+                                                    type="email"
+                                                    autocomplete="email"
+                                                    v-model="form.email"
+                                                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                                />
+                                                <span
+                                                    className="text-red-600"
+                                                    v-if="form.errors.email"
+                                                >
+                                                    {{ form.errors.email }}
+                                                </span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div
+                                class="mt-6 flex items-center justify-end gap-x-6"
+                            >
+                                <button
+                                    type="button"
+                                    class="text-sm font-semibold leading-6 text-gray-900"
+                                >
+                                    Cancel
+                                </button>
+                                <button
+                                    type="submit"
+                                    class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                >
+                                    Save
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
---
+++ resources/js/Pages/Doctors/Show.vue
@@ -0,0 +1,139 @@
+<script setup>
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+import { Head, Link } from "@inertiajs/vue3";
+
+defineProps({
+    doctor: Object,
+});
+</script>
+
+<template>
+    <Head title="Doctor" />
+    <AuthenticatedLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Doctor
+            </h2>
+        </template>
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white shadow overflow-hidden sm:rounded-lg">
+                    <div class="p-6 bg-white border-b border-gray-200">
+                        <div className="flex items-center justify-between mb-6">
+                            <Link
+                                className="px-4 py-2 ml-5 text-white bg-blue-500 rounded-md focus:outline-none"
+                                :href="route('doctors.index')"
+                            >
+                                Back
+                            </Link>
+                        </div>
+                        <div class="px-4 py-5 sm:px-6">
+                            <h3
+                                class="text-lg leading-6 font-medium text-gray-900"
+                            >
+                                Doctor Details
+                            </h3>
+                            <p class="mt-1 max-w-2xl text-sm text-gray-500">
+                                View detailed information about the doctor.
+                            </p>
+                        </div>
+                        <div class="border-t border-gray-200">
+                            <dl>
+                                <div
+                                    class="bg-gray-100 px-4 py-2 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6"
+                                >
+                                    <dt
+                                        class="text-sm font-medium text-gray-500"
+                                    >
+                                        ID
+                                    </dt>
+                                    <dd
+                                        class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2"
+                                    >
+                                        :
+                                        {{ doctor.id }}
+                                    </dd>
+                                </div>
+                                <div
+                                    class="bg-white px-4 py-2 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6"
+                                >
+                                    <dt
+                                        class="text-sm font-medium text-gray-500"
+                                    >
+                                        First Name
+                                    </dt>
+                                    <dd
+                                        class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2"
+                                    >
+                                        :
+                                        {{ doctor.first_name }}
+                                    </dd>
+                                </div>
+                                <div
+                                    class="bg-gray-100 px-4 py-2 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6"
+                                >
+                                    <dt
+                                        class="text-sm font-medium text-gray-500"
+                                    >
+                                        Last Name
+                                    </dt>
+                                    <dd
+                                        class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2"
+                                    >
+                                        :
+                                        {{ doctor.last_name }}
+                                    </dd>
+                                </div>
+                                <div
+                                    class="bg-white px-4 py-2 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6"
+                                >
+                                    <dt
+                                        class="text-sm font-medium text-gray-500"
+                                    >
+                                        Specialization
+                                    </dt>
+                                    <dd
+                                        class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2"
+                                    >
+                                        :
+                                        {{ doctor.specialization }}
+                                    </dd>
+                                </div>
+                                <div
+                                    class="bg-gray-100 px-4 py-2 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6"
+                                >
+                                    <dt
+                                        class="text-sm font-medium text-gray-500"
+                                    >
+                                        Phone Number
+                                    </dt>
+                                    <dd
+                                        class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2"
+                                    >
+                                        :
+                                        {{ doctor.phone_number }}
+                                    </dd>
+                                </div>
+                                <div
+                                    class="bg-white px-4 py-2 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6"
+                                >
+                                    <dt
+                                        class="text-sm font-medium text-gray-500"
+                                    >
+                                        Email Address
+                                    </dt>
+                                    <dd
+                                        class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2"
+                                    >
+                                        :
+                                        {{ doctor.email }}
+                                    </dd>
+                                </div>
+                            </dl>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
